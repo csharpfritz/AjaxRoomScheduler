2f6d779312d8a1f172500b8d1636adb7324eb36a
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2012-12-10 17:18:50 -0500

    Initial layout and master page construction

---
+++ Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" MasterPageFile="~/MasterPage.master" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="head">
+
+</asp:Content>
+
+<asp:Content runat="server" ContentPlaceHolderID="body">
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+
+public partial class Default : System.Web.UI.Page 
+{
+
+    protected override void OnInit(EventArgs e)
+    {
+
+        Context.Items["Title"] = "Home";
+        
+        base.OnInit(e);
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
